0106aa219677d3a08c951caf35116e2b763d8401
Nutti Saelor <leelorz6@gmail.com>
2018-03-26 01:00:57 +0700

    add async Redux

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,7 +1,25 @@
 // @flow
 
-import { SET_SEARCH_TERM } from './action';
+import axios from 'axios';
+import { SET_SEARCH_TERM, ADD_API_DATA } from './action';
 
 export function setSearchTerm(searchTerm: string) {
   return { type: SET_SEARCH_TERM, payload: searchTerm };
+}
+
+export function addAPIData(apiData: Show) {
+  return { type: ADD_API_DATA, payload: apiData };
+}
+
+export function getAPIDetail(imdbID: string) {
+  return (dispatch: Function) => {
+    axios
+      .get(`http://localhost:3000/${imdbID}`)
+      .then(response => {
+        dispatch(addAPIData(response.data));
+      })
+      .catch(error => {
+        console.error('axios error', error); // eslint-disable-line no-console
+      });
+  };
 }
