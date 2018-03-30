7e4a54f25ade6339a00a8bc730a04e6694b6cd12
Nutti Saelor <leelorz6@gmail.com>
2018-03-31 02:00:29 +0700

    add code splitting

--- js/App.jsx
+++ js/App.jsx
@@ -3,23 +3,32 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import store from './store'
-import Landing from './Landing';
-import Search from './Search';
-import Details from './Details';
+import { Provider } from 'react-redux';
+import store from './store';
+import AsyncRoute from './AsyncRoute';
 import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
-    <Provider store={store}>
+  <Provider store={store}>
     <div className="app">
       <Switch>
-        <Route exact path="/" component={Landing} />
+        <Route
+          exact
+          path="/"
+          component={props => (
+            <AsyncRoute props={props} loadingPromise={import('./Landing')} />
+          )}
+        />
         <Route
           path="/search"
-          component={props => <Search shows={preload.shows} {...props} />}
+          component={props => (
+            <AsyncRoute
+              props={Object.assign({ shows: preload.shows }, props)}
+              loadingPromise={import('./Search')}
+            />
+          )}
         />
         <Route
           path="/details/:id"
@@ -27,13 +36,18 @@ const App = () => (
             const selectedShow = preload.shows.find(
               show => props.match.params.id === show.imdbID
             );
-            return <Details show={selectedShow} {...props} />;
+            return (
+              <AsyncRoute
+                props={Object.assign({ show: selectedShow, match: {} }, props)}
+                loadingPromise={import('./Details')}
+              />
+            );
           }}
         />
         <Route component={FourOhFour} />
       </Switch>
     </div>
-    </Provider>
+  </Provider>
 );
 
 export default App;
